67822c8ae6824771511ccf112922676c15cd769f
avicnotes <78015849+avicnotes@users.noreply.github.com>
2021-11-29 14:29:31 +0300

    add testcomponent

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <HelloWorld msg="Hello My First Vue application!"/>
+    <TestComp/>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
-
+import TestComp from "./components/TestComp"
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    TestComp
   }
 };
 </script>
